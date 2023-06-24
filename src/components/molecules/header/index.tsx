f7c4a2bb9e4128e3732007fa0e71930f7199ca4d
Nicolas R Almeida <nicolas_ariel_almeida@outlook.com>
2023-06-24 01:01:47 -0300

    add: pdfs

--- src/components/molecules/header/index.tsx
+++ src/components/molecules/header/index.tsx
@@ -1,9 +1,8 @@
 "use client"
-import Button from '@/components/atoms/button'
-import Hero from 'public/hero.jpg'
-import { useState } from 'react'
-import MobileMenu from '../menu'
-import { AStyle, HeaderStyle, MenuStyle } from './styled'
+import Button from '@/components/atoms/button';
+import { useState } from 'react';
+import MobileMenu from '../menu';
+import { AStyle, HeaderStyle, MenuStyle } from './styled';
 
 export default function Header() {
    const [isActiveMenu, setIsActiveMenu] = useState(false)
@@ -12,21 +11,17 @@ export default function Header() {
       <HeaderStyle>
          <MobileMenu active={open} isopen={isActiveMenu}></MobileMenu>
          <MenuStyle>
-            <AStyle href={Hero.src} download="presentes">
+            <AStyle href="/Nos.pdf" download="breve_resumo">
                NOSSA HISTORIA
             </AStyle>
-            <a
-               target="_blank"
-               href="https://www.google.com/calendar/render?action=TEMPLATE&text=Casamento+Nicolas+%26+Thaina&location=Mans%C3%A3o+Arya&dates=20230709T133000Z%2F20230709T190000Z"
-            ></a>
             <AStyle
                target="_blank"
                href="https://www.google.com/calendar/render?action=TEMPLATE&text=Casamento+Nicolas+%26+Thaina&location=Mans%C3%A3o+Arya&dates=20230709T133000Z%2F20230709T190000Z"
             >
                MARCAR NA AGENDA
             </AStyle>
-            <AStyle href="">PRESENTES</AStyle>
-            <AStyle href="">ALBUM</AStyle>
+            <AStyle href="/presentes.pdf" download="presentes">PRESENTES</AStyle>
+            <AStyle href="https://drive.google.com/drive/folders/1uyTzA_tQcFOTWbcQ5IEDc_36sdo-ONxk?usp=sharing">ALBUM</AStyle>
          </MenuStyle>
          <Button
             link={
